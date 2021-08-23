38d8620b5b1f76ab3cd55a0b8955cb376967886c
j0ker404 <uanhermanus@gmail.com>
2021-08-23 16:50:49 +0200

    add time step counter

--- alphaslime/evaluate/eval_agents.py
+++ alphaslime/evaluate/eval_agents.py
@@ -42,6 +42,7 @@ class EvaluateGame:
                 - save state
                 - save actions
                 - save rewards
+                - save time step
         '''
         obs1 = self.env.reset()
         obs2 = obs1 # both sides always see the same initial observation.
@@ -49,17 +50,26 @@ class EvaluateGame:
         done = False
         total_reward = 0
 
+        # time step counter
+        t = 0
         # start episode
         while not done:
 
             action_right = self.agent_right.get_action(obs1)
             action_left = self.agent_left.get_action(obs2)
+            # print('action_right = {}'.format(action_right))
+            # print('action_left = {}'.format(action_left))
 
+            # print('t = {}'.format(t))
+
+            # go to next time step
             obs1, reward, done, info = self.env.step(action_right, action_left) # extra argument
             obs2 = info['otherObs'] #  opponent's observations
 
             total_reward += reward
 
+            # increment time step
+            t += 1
             if self.RENDER:
                 # render game to screen
                 self.env.render()
