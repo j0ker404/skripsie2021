65fddc8637776638278c2ea6dd36622b77c17ba5
j0ker404 <uanhermanus@gmail.com>
2021-08-25 16:54:41 +0200

    add default action table to agent class

--- alphaslime/agents/agent.py
+++ alphaslime/agents/agent.py
@@ -12,6 +12,12 @@ class Agent:
         '''
         # self.actions = np.array([0,0,0])
         self.actions = [0,0,0]
+        self.action_table = [[0, 0, 0], # NOOP
+                            [1, 0, 0], # LEFT (forward)
+                            [1, 0, 1], # UPLEFT (forward jump)
+                            [0, 0, 1], # UP (jump)
+                            [0, 1, 1], # UPRIGHT (backward jump)
+                            [0, 1, 0]] # RIGHT (backward)
 
     def train(self):
         '''
