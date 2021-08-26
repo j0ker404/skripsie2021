b722954a8adabeea152510b68f189c8d9b65826a
j0ker404 <uanhermanus@gmail.com>
2021-08-26 02:35:15 +0200

    simple wrapper class

---
+++ alphaslime/envgame/slenv.py
@@ -0,0 +1,51 @@
+'''
+    Single Agent Game environment wrapper
+
+    Useful when trainng a single agent against a baseline agent
+
+    Can be useful for self play
+'''
+
+
+import numpy as np
+import gym
+from ..agents.agent import Agent 
+
+class SLenv:
+
+    def __init__(self,opponent:Agent, env_id="SlimeVolley-v0") -> None:
+        
+        # opponent agent: left player
+        self.opponent = opponent
+
+        # set environment
+        self.env = gym.make(env_id)
+        
+
+    def reset(self):
+        '''
+            Reset game 
+        '''
+        self.obs_player =  self.env.reset()
+        self.obs_opponent = self.obs_player
+
+        return self.obs_player
+
+    def step(self, action):
+        '''
+            Make action and return reward, next state, etc
+        '''
+        action_opponent = self.opponent.get_action(self.obs_opponent)
+        self.obs_player, reward, done, info = self.env.step(action, action_opponent)
+        self.obs_opponent = info['otherObs']
+        
+
+        return self.obs_player, reward, done, info
+
+
+
+    
+        
+
+
+
