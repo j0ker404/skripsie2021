ff799affb0d59a8b569305e92a0ee07bbafcdcf6
j0ker404 <uanhermanus@gmail.com>
2021-08-30 17:18:56 +0200

    display data to console

--- test_agents.py
+++ test_agents.py
@@ -1,18 +1,47 @@
 from alphaslime.evaluate.eval_agents import EvaluateGame
 from alphaslime.agents.baseline import BaselineAgent
+from alphaslime.agents.selfplay.semiGradSarsa import SemiGradSarsa
+
+from pandas import read_csv
+import numpy as np
 
 if __name__ == '__main__':
-    agent_right = BaselineAgent()
+
+    df = read_csv('./train/weights_sarsa.csv', header=None)
+    weights = df.values
+    print(weights)
+
+    agent_right = SemiGradSarsa(epsilon=0.0, weights=weights)
     agent_left = BaselineAgent()
     base_dir = './'
-    RENDER = True
+    RENDER = False
     env_id = "SlimeVolley-v0"
     # env_id = "SlimeVolleyPixel-v0"
     eval_game = EvaluateGame(agent_right=agent_right, agent_left=agent_left, base_dir_path=base_dir, render=RENDER, env_id=env_id)
 
+    N = 50
     # run N episodes
-    N = 1
+    agent_right_score = np.zeros((N,))
+    agent_left_score = np.zeros((N,))
     print('Start running episodes..')
     for n in range(N):
-        print('Evaulating episode {}'.format(n))
-        eval_game.evaluate_episode()
+        # print('-'*99)
+        # print('Evaulating episode {}'.format(n))
+        score = eval_game.evaluate_episode()
+        agent_right_score[n] = score
+        agent_left_score[n] = -score
+        # print('Agent right  score = {}'.format(score))
+        # print('Agent left  score = {}'.format(-score))
+        # print('-'*99)
+    
+
+    print('*'*10)
+    print('Agent right total score = {}'.format(agent_right_score.sum()))
+    print('Agent left total score = {}'.format(agent_left_score.sum()))
+    print('*'*10)
+
+    print('*'*10)
+    print('Agent(player) score: \n')
+    for episode, score in enumerate(agent_right_score):
+        print('Episode {}\t: {}'.format(episode, score))
+    print('*'*10)
