47b0bd9685320c4f426b7161b732eee096814458
j0ker404 <uanhermanus@gmail.com>
2021-09-03 01:16:22 +0200

    inint

--- other/cartpole/test_agent.py
+++ other/cartpole/test_agent.py
@@ -1,25 +1,39 @@
+import sys
+import os
+
+PACKAGE_PARENT = '../../'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
 from alphaslime.evaluate.eval_agents import EvaluateGameSA
 from alphaslime.agents.other.semiGradSarsa import SemiGradSarsa
 
 from pandas import read_csv
 import numpy as np
 
+
+
+
 if __name__ == '__main__':
 
     # df = read_csv('./train/weights_sarsa.csv', header=None)
-    df = read_csv('./other/train/sarsa/weight.csv', header=None)
+    df = read_csv('./other/cartpole/train/sarsa/weight.csv', header=None)
     weights = df.values
     print(weights)
 
     # agent_right = SemiGradSarsa(epsilon=0.0, weights=weights)
-    agent = SemiGradSarsa(epsilon=0.0, weights=weights)
-    base_dir = './'
-    RENDER = True
-    env_id = "SlimeVolley-v0"
-    # env_id = "SlimeVolleyPixel-v0"
-    eval_game = EvaluateGameSA(agent==agent, base_dir_path=base_dir, render=RENDER, env_id=env_id)
+    # agent = SemiGradSarsa(epsilon=0.0, weights=weights)
+    agent = SemiGradSarsa(epsilon=0.0, weights=weights, is_MA=False, env_id='CartPole-v1')
+    agent.action_table =  [0, 1]
+    agent.max_actions = len(agent.action_table)
+    agent.MAX_SCORE = 195
 
-    N = 50
+    base_dir = './'
+    RENDER = False
+    env_id = "CartPole-v0"
+    eval_game = EvaluateGameSA(agent=agent, base_dir_path=base_dir, render=RENDER, env_id=env_id)
+
+    N = 10
     # run N episodes
     agent_right_score = np.zeros((N,))
     print('Start running episodes..')
