ea6d1c6ee8a5d19aee694e441dda5a86d5ec761c
j0ker404 <uanhermanus@gmail.com>
2021-10-05 02:23:39 +0200

    create base epsilon decay class

---
+++ alphaslime/epsilon/epsilon.py
@@ -0,0 +1,15 @@
+class Epsilon:
+    '''
+        Base epislon decay class
+    '''
+
+    def __init__(self) -> None:
+        pass
+
+    def get_epsilon(self, episode):
+        """Epsilon decay base method
+
+        Args:
+            episode (int): current episode iteration
+        """
+        pass
