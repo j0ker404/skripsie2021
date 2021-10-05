9e9ea79eb2a662716ffca9c5dd01e878f8d69001
j0ker404 <uanhermanus@gmail.com>
2021-10-05 12:13:39 +0200

    DQN implementation

---
+++ alphaslime/approx/dqn.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+class DQN(nn.Module):
+    """Deep Q network class
+
+    Args:
+        nn ([torch.nn]): NN base class
+    """
+
+    def __init__(self, lr, device, layer_sizes):
+        super().__init__()
+        self.layer_sizes = layer_sizes
+        self.input_size = self.layer_sizes[0]
+        self.hidden_layer_size = self.layer_sizes[1]
+        self.output_size = self.layer_sizes[2]
+        self.seq = nn.Sequential(
+            nn.Linear(self.input_size, self.hidden_layer_size),
+            nn.PReLU(),
+            nn.Linear(self.hidden_layer_size, self.hidden_layer_size),
+            nn.PReLU(),
+            nn.Linear(self.hidden_layer_size, self.hidden_layer_size),
+            nn.PReLU(),
+            nn.Linear(self.hidden_layer_size, self.output_size)
+        )
+        self.learning_rate = lr
+        self.device = device
+
+    def forward(self, x):
+        logits = self.seq(x)
+        return logits
