52151b72a175d6730ca2cec46af096ebc17748e1
j0ker404 <uanhermanus@gmail.com>
2021-10-12 12:31:33 +0200

    QLearning base file name generator

---
+++ alphaslime/trainer/datahelp/q_agents.py
@@ -0,0 +1,28 @@
+from torch.functional import Tensor
+from alphaslime.trainer.datahelp.filename import FileName
+from alphaslime.agents.agent import Agent
+import torch
+
+class QLearnFile(FileName):
+    """Class that generates the base file name
+
+
+    Args:
+        FileName (FileName): Base Class
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def gen_name(self, agent:Agent, prefix=''):
+        gamma = agent.gamma
+        if type(gamma) == torch.Tensor:
+            gamma = gamma.item()
+        learning_rate = agent.lr
+
+        if type(learning_rate) == torch.Tensor:
+            learning_rate = learning_rate.item()
+
+        avg_reward = agent.avg_rewards[-1]
+        file_info = "gamma_{}_lr_rate_{}_reward_{}".format(str(gamma), str(learning_rate), str(avg_reward)) 
+        return file_info
