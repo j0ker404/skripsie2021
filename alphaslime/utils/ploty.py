c4278c5e7bcafad31ce63a96d90daaffe0f4e6ff
j0ker404 <uanhermanus@gmail.com>
2021-10-19 16:07:22 +0200

    add labels

--- alphaslime/utils/ploty.py
+++ alphaslime/utils/ploty.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_learning_curve_plus_score(x, scores, figure_file, running_avg_len=100):
+def plot_learning_curve_plus_score(x, scores, figure_file, running_avg_len=100, title='Running average'):
     """Plot the total episode rewards as well as the running episode average
 
         Plot will be saved to disk
@@ -16,8 +16,11 @@ def plot_learning_curve_plus_score(x, scores, figure_file, running_avg_len=100):
     running_avg = np.zeros(len(scores))
     for i in range(len(running_avg)):
         running_avg[i] = np.mean(scores[max(0, i-running_avg_len):(i+1)])
-    plt.plot(scores)
-    plt.plot(x, running_avg)
-    plt.title('Running average of previous 100 scores')
+    plt.plot(x, scores, label='Total Reward per episode')
+    plt.plot(x, running_avg, label='Running total reward average over {} episodes'.format(running_avg_len))
+    plt.xlabel('Episodes')
+    plt.ylabel('Total Reward per episode')
+    plt.legend(bbox_to_anchor=(1.05, 1)) 
+    plt.title(title)
     plt.savefig(figure_file)
     plt.show()
