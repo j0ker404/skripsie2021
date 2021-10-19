d59e79027b36bd600012b8d11b7f3a798e32a870
j0ker404 <uanhermanus@gmail.com>
2021-10-19 16:06:41 +0200

    refactor intro seprate files

---
+++ alphaslime/agents/RL/policygrad/torch/critic.py
@@ -0,0 +1,46 @@
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.categorical import Categorical
+
+class CriticNetwork(nn.Module):
+    def __init__(self, layer_sizes, alpha):
+        super(CriticNetwork, self).__init__()
+        input_dim = layer_sizes[0]
+        layer1_dim = layer_sizes[1]
+        layer2_dim = layer_sizes[2]
+        output_dim = 1
+        self.critic = nn.Sequential(
+                nn.Linear(input_dim, layer1_dim),
+                nn.ReLU(),
+                nn.Linear(layer1_dim, layer2_dim),
+                nn.ReLU(),
+                nn.Linear(layer2_dim, output_dim)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        value = self.critic(state)
+
+        return value
+   
+    def save_model(self, path):
+        """Save NN model to disk
+
+        Args:
+            path (str): file path to save file
+                        in the form of: $PATH$.pt
+        """
+        T.save(self.state_dict(), path)
+
+    def load_model(self, path):
+        """Load NN model from disk
+
+        Args:
+            path (str): file path to load file
+                        in the form of: $PATH$.pt
+        """
+        self.load_state_dict(T.load(path))
--- alphaslime/agents/RL/policygrad/torch/ppo.py
+++ alphaslime/agents/RL/policygrad/torch/ppo.py
@@ -2,149 +2,16 @@ from collections import deque
 import os
 import numpy as np
 import torch as T
-import torch.nn as nn
-from torch.nn.modules import loss
-import torch.optim as optim
-from torch.distributions.categorical import Categorical
 from tqdm import tqdm
+from alphaslime.agents.RL.policygrad.torch.actor import ActorNetwork
+from alphaslime.agents.RL.policygrad.torch.critic import CriticNetwork
+from alphaslime.agents.RL.policygrad.torch.memory import PPOMemory
 
 from alphaslime.agents.agent import Agent
 from alphaslime.store.config import Config
 
 
-class PPOMemory:
-    def __init__(self, batch_size):
-        self.states = []
-        self.probs = []
-        self.vals = []
-        self.actions = []
-        self.rewards = []
-        self.dones = []
 
-        self.batch_size = batch_size
-
-    def generate_batches(self):
-        n_states = len(self.states)
-        batch_start = np.arange(0, n_states, self.batch_size)
-        indices = np.arange(n_states, dtype=np.int64)
-        np.random.shuffle(indices)
-        batches = [indices[i:i+self.batch_size] for i in batch_start]
-
-        return np.array(self.states),\
-                np.array(self.actions),\
-                np.array(self.probs),\
-                np.array(self.vals),\
-                np.array(self.rewards),\
-                np.array(self.dones),\
-                batches
-
-    def store_memory(self, state, action, probs, vals, reward, done):
-        self.states.append(state)
-        self.actions.append(action)
-        self.probs.append(probs)
-        self.vals.append(vals)
-        self.rewards.append(reward)
-        self.dones.append(done)
-
-    def clear_memory(self):
-        self.states = []
-        self.probs = []
-        self.actions = []
-        self.rewards = []
-        self.dones = []
-        self.vals = []
-
-class ActorNetwork(nn.Module):
-    def __init__(self, n_actions, layer_sizes, alpha):
-    # def __init__(self, n_actions, input_dims, alpha,
-    #         fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
-        super(ActorNetwork, self).__init__()
-        input_dim = layer_sizes[0]
-        layer1_dim = layer_sizes[1]
-        layer2_dim = layer_sizes[2]
-        output_dim = n_actions
-        # self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
-        self.actor = nn.Sequential(
-                nn.Linear(input_dim, layer1_dim),
-                nn.ReLU(),
-                nn.Linear(layer1_dim, layer2_dim),
-                nn.ReLU(),
-                nn.Linear(layer2_dim, output_dim),
-                nn.Softmax(dim=-1)
-        )
-
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
-        self.to(self.device)
-
-    def forward(self, state):
-        dist = self.actor(state)
-        dist = Categorical(dist)
-        
-        return dist
-    
-    def save_model(self, path):
-        """Save NN model to disk
-
-        Args:
-            path (str): file path to save file
-                        in the form of: $PATH$.pt
-        """
-        T.save(self.state_dict(), path)
-
-    def load_model(self, path):
-        """Load NN model from disk
-
-        Args:
-            path (str): file path to load file
-                        in the form of: $PATH$.pt
-        """
-        self.load_state_dict(T.load(path))
-
-class CriticNetwork(nn.Module):
-    def __init__(self, layer_sizes, alpha):
-    # def __init__(self, input_dims, alpha, fc1_dims=256, fc2_dims=256,
-    #         chkpt_dir='tmp/ppo'):
-        super(CriticNetwork, self).__init__()
-        input_dim = layer_sizes[0]
-        layer1_dim = layer_sizes[1]
-        layer2_dim = layer_sizes[2]
-        output_dim = 1
-        # self.checkpoint_file = os.path.join(chkpt_dir, 'critic_torch_ppo')
-        self.critic = nn.Sequential(
-                nn.Linear(input_dim, layer1_dim),
-                nn.ReLU(),
-                nn.Linear(layer1_dim, layer2_dim),
-                nn.ReLU(),
-                nn.Linear(layer2_dim, output_dim)
-        )
-
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
-        self.to(self.device)
-
-    def forward(self, state):
-        value = self.critic(state)
-
-        return value
-   
-    def save_model(self, path):
-        """Save NN model to disk
-
-        Args:
-            path (str): file path to save file
-                        in the form of: $PATH$.pt
-        """
-        T.save(self.state_dict(), path)
-
-    def load_model(self, path):
-        """Load NN model from disk
-
-        Args:
-            path (str): file path to load file
-                        in the form of: $PATH$.pt
-        """
-        self.load_state_dict(T.load(path))
 
 class PPOAgent(Agent):
 
