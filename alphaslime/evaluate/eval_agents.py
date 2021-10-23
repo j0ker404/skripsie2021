6b0161239b579c5e9f03406875b7ec7f6a5ca5ae
j0ker404 <uanhermanus@gmail.com>
2021-10-23 20:08:34 +0200

    Refactor by extending base class

--- alphaslime/evaluate/eval_agents.py
+++ alphaslime/evaluate/eval_agents.py
@@ -10,14 +10,14 @@
 
 
 '''
-from collections import deque
-import numpy as np
 from torch.functional import Tensor
 from alphaslime.agents.agent import Agent
 import time
-from tqdm import tqdm
 
-class EvaluateGameMA:
+
+from alphaslime.evaluate.eval import Evaluate
+
+class EvaluateGameMA(Evaluate):
     '''
         Multi-agent evalutaion
 
@@ -27,13 +27,10 @@ class EvaluateGameMA:
         #TODO: add functionality for human controlled agent
     '''
     def __init__(self, agent_right:Agent, agent_left:Agent, env, base_dir_path, render=False, time_delay=0.03) -> None:
+        super().__init__(env, base_dir_path, render=render, time_delay=time_delay)
         self.agent_right = agent_right
         self.agent_left = agent_left
-        self.RENDER = render
-        # base directory to save data
-        self.base_dir_path = base_dir_path
-        self.env = env
-        self.delay = time_delay
+
     
     def evaluate_episode(self):
         '''
@@ -123,39 +120,8 @@ class EvaluateGameMA:
 
         return total_reward
 
-    def evaluate(self, EPISODES, is_progress_bar=False, running_avg_len=100):
-        """Evaluate agent performance for given number of episodes
 
-        Args:
-            EPISODES (int): Number of Episodes to run
-            is_progress_bar (bool): Display progress bar. Default to False
-        
-        return:
-            episodes_reward: (list), total reward per episode
-            avg_rewards_array: (list), running reward average per episode 
-        """
-        rewards = []
-        rewards_deque = deque(maxlen=running_avg_len)
-        avg_rewards_array = [] 
-        ranger = range(EPISODES)
-        if is_progress_bar:
-            ranger = tqdm(ranger)
-        
-        # evaluate episodes
-        for episode in ranger:
-            episode_reward = self.evaluate_episode()
-            rewards_deque.append(episode_reward)
-            avg_score = np.mean(rewards_deque)
-            # append average reward at current epsiode
-            avg_rewards_array.append(avg_score)
-            # append total episode reward
-            rewards.append(episode_reward)
-
-        self.env.close()
-
-        return rewards, avg_rewards_array
-
-class EvaluateGameSA:
+class EvaluateGameSA(Evaluate):
     '''
         Single-agent evalutaion
 
@@ -174,12 +140,8 @@ class EvaluateGameSA:
             render (bool, optional): Determine if agent-environment interaction must be drawn to screen. Defaults to False.
             time_delay (float, optional): Time delay between each frame of Rendered interaction. Defaults to 0.03.
         """
+        super().__init__(env, base_dir_path, render=render, time_delay=time_delay)
         self.agent = agent
-        self.RENDER = render
-        # base directory to save data
-        self.base_dir_path = base_dir_path
-        self.env = env
-        self.delay = time_delay
 
     
     def evaluate_episode(self):
@@ -237,38 +199,3 @@ class EvaluateGameSA:
                 # sleep
                 time.sleep(self.delay)
         return total_reward
-
-    def evaluate(self, EPISODES, is_progress_bar=False, running_avg_len=100):
-        """Evaluate agent performance for given number of episodes
-
-        Args:
-            EPISODES (int): Number of Episodes to run
-            is_progress_bar (bool): Display progress bar. Default to False
-        
-        return:
-            episodes_reward: (list), total reward per episode
-            avg_rewards_array: (list), running reward average per episode 
-        """
-        rewards = []
-        rewards_deque = deque(maxlen=running_avg_len)
-        avg_rewards_array = [] 
-        ranger = range(EPISODES)
-        if is_progress_bar:
-            ranger = tqdm(ranger)
-        
-        # evaluate episodes
-        for episode in ranger:
-            episode_reward = self.evaluate_episode()
-            rewards_deque.append(episode_reward)
-            avg_score = np.mean(rewards_deque)
-            # append average reward at current epsiode
-            avg_rewards_array.append(avg_score)
-            # append total episode reward
-            rewards.append(episode_reward)
-
-        self.env.close()
-
-        return rewards, avg_rewards_array
-
-
-
