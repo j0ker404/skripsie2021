478c8cbfdad6edbae4705ec42fa862e1c14ee98b
j0ker404 <uanhermanus@gmail.com>
2021-10-27 01:23:36 +0200

    return action index instead of action

--- alphaslime/agents/baseline.py
+++ alphaslime/agents/baseline.py
@@ -14,9 +14,17 @@ class BaselineAgent(Agent):
         self.policy = slimevolleygym.BaselinePolicy() 
 
     def get_action(self, state):
+        """Return index of action that matches the 
+
+        Args:
+            state (list): state observed
+
+        Returns:
+            action_index: index of action that matches action_table
+        """
         # action =  super().get_action(state)
         self.actions  = self.policy.predict(state)
-
-        return self.actions
+        action_index = self.action_table.index(self.actions )
+        return action_index
 
 
